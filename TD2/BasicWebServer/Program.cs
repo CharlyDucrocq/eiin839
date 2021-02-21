aac2d23dbba5839436a1fac36dabd94155f9a7b8
CharlyDucrocq <charly.ducrocq@etu.univ-cotedazur.fr>
2021-02-21 15:19:57 +0100

    TP2

--- TD2/BasicWebServer/Program.cs
+++ TD2/BasicWebServer/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Web;
-using BasicWebServer;
 
 namespace BasicServerHTTPlistener
 {
@@ -103,9 +104,6 @@ namespace BasicServerHTTPlistener
                 Console.WriteLine("param3 = " + HttpUtility.ParseQueryString(request.Url.Query).Get("param3"));
                 Console.WriteLine("param4 = " + HttpUtility.ParseQueryString(request.Url.Query).Get("param4"));
 
-
-                Header header = new Header(request);
-
                 //
                 Console.WriteLine(documentContents);
 
@@ -113,7 +111,44 @@ namespace BasicServerHTTPlistener
                 HttpListenerResponse response = context.Response;
 
                 // Construct a response.
-                string responseString = "Le header est le suivant : \n"+header;
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+
+                string lastSegment = request.Url.Segments[request.Url.Segments.Length - 1];
+                Type type = typeof(MyReflectionClass);
+                MethodInfo method = type.GetMethod(lastSegment);
+
+                if (method != null)
+                {
+                    Console.WriteLine("MyMethodDetected");
+
+                    MyReflectionClass c = new MyReflectionClass();
+                    responseString = (string)method.Invoke(c, new string[2] { HttpUtility.ParseQueryString(request.Url.Query).Get("param1"), HttpUtility.ParseQueryString(request.Url.Query).Get("param2") });
+                }
+                else
+                {
+                    if (lastSegment.Equals("externalMethod"))
+                    {
+                        ProcessStartInfo start = new ProcessStartInfo();
+                        start.FileName = @"C:\Users\ducch\Desktop\Bossage\Tp-SOC\eiin839\TD2\ExecTest\bin\Debug\ExecTest.exe"; // Specify exe name.
+                        start.Arguments = HttpUtility.ParseQueryString(request.Url.Query).Get("param1"); // Specify arguments.
+                        start.UseShellExecute = false;
+                        start.RedirectStandardOutput = true;
+                        //
+                        // Start the process.
+                        //
+                        using (Process process = Process.Start(start))
+                        {
+                            //
+                            // Read in all the text from the process with the StreamReader.
+                            //
+                            using (StreamReader reader = process.StandardOutput)
+                            {
+                                responseString = reader.ReadToEnd();
+                            }
+                        }
+                    } else Console.WriteLine("MyMethodNotDetected");
+                }
+
                 byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
                 // Get a response stream and write the response to it.
                 response.ContentLength64 = buffer.Length;
@@ -124,6 +159,14 @@ namespace BasicServerHTTPlistener
             }
             // Httplistener neither stop ... But Ctrl-C do that ...
             // listener.Stop();
+        }
+    }
+
+    public class MyReflectionClass
+    {
+        public string methodReflection(string p1, string p2)
+        {
+            return "<HTML><BODY> Hello " + p1 + " et " + p2 + "</BODY></HTML>";
         }
     }
 }
