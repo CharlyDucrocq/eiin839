1404949c85c2eae08da6862f4928146176c4d86d
CharlyDucrocq <charly.ducrocq@etu.univ-cotedazur.fr>
2021-03-31 14:59:33 +0200

    TP6

--- TD3/SimpleRestServeur/SimpleRestServeur/Program.cs
+++ TD3/SimpleRestServeur/SimpleRestServeur/Program.cs
@@ -13,51 +13,6 @@ namespace BasicServerHTTPlistener
 
         static async Task Main(string[] args)
         {
-            // Call asynchronous network methods in a try/catch block to handle exceptions.
-            try
-            {
-                switch (args.Length){
-                    case 0 :
-                            HttpResponseMessage response = await client.GetAsync("https://api.jcdecaux.com/vls/v3/contracts?apiKey=f885f76c599180bf47a6cb6a88c3c1ee83d9ed66");
-                            response.EnsureSuccessStatusCode();
-                            string responseBody = await response.Content.ReadAsStringAsync();
-                            Object[] contracts = JsonSerializer.Deserialize<Contract[]>(responseBody);
-
-                            // Above three lines can be replaced with new helper method below
-                            // string responseBody = await client.GetStringAsync(uri);
-                            foreach (Contract contract in contracts)
-                            {
-                                Console.WriteLine(contract);
-                            }
-                        break;
-                    case 1:
-                        HttpResponseMessage response2 = await client.GetAsync("https://api.jcdecaux.com/vls/v1/stations?contract=" + args[0] + "&apiKey=f885f76c599180bf47a6cb6a88c3c1ee83d9ed66");
-                        response2.EnsureSuccessStatusCode();
-                        string responseBody2 = await response2.Content.ReadAsStringAsync();
-                        Object[] stations = JsonSerializer.Deserialize<Contract[]>(responseBody2);
-
-                        // Above three lines can be replaced with new helper method below
-                        // string responseBody = await client.GetStringAsync(uri);
-                        foreach (Contract contract in stations)
-                        {
-                            Console.WriteLine(contract);
-                        }
-                        break;
-                    case 2:
-                        HttpResponseMessage response3 = await client.GetAsync("https://api.jcdecaux.com/vls/v1/stations/"+args[1]+"?contract=" + args[0] + "&apiKey=f885f76c599180bf47a6cb6a88c3c1ee83d9ed66");
-                        response3.EnsureSuccessStatusCode();
-                        string responseBody3 = await response3.Content.ReadAsStringAsync();
-
-                        Console.WriteLine(responseBody3);
-                        break;
-                }
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-            }
-
             CalculatorSoapClient calculator = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
 
             Console.WriteLine("1+2=" + calculator.AddAsync(1,2).Result);
